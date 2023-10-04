08b2567ce0d31a8ec01b9ae710a525c0602a7eed
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-10-04 11:09:09 +0530

    responsive navbar doen

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const iconBtn = document.querySelector(".icon")
+const  toggleBtn = document.querySelector(".icon")
 
-let displayNav = false;
-
-const responsiveNavbar = () => {
-    displayNav = !displayNav;
-    const navBar = document.querySelector(".nav-links");
-    if(displayNav) {
-        navBar.style.display = "block";
-    }
-    else {
-        navBar.style.display = "none";
-    }
-
-}
-
-iconBtn.addEventListener("click", responsiveNavbar)
+toggleBtn.addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("nav-links-display")
+})
